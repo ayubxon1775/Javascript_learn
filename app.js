53d0776f876f3c12dccb1d98d0916e7099e968ef
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-04-18 12:10:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// 2.2 masala
+
+// let n = Number(prompt('32 dan katt 126 ga teng yoki kichik bolgan ihtiyoriy butun son kiriting'));
+
+function findCharacter(){
+
+    if(n > 32 && n <= 126){
+        let character = String.fromCharCode(n);
+        return character;
+    }
+}
+// console.log(findCharacter());
+
+
+
